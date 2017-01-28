1ef2d63fc360f2d0c55f5d072053b96c854b6b57
jtudruj <jakub.tudruj@pollub.edu.pl>
2017-01-28 14:34:20 +0100

    initial commit

---
+++ lab02/src/java/pl/pollub/Helper.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.pollub;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ *
+ * @author linuxlite
+ */
+public class Helper {
+    public static void writeResults(HashMap results) {
+        try {
+            FileWriter fw = new FileWriter("wyniki.txt");
+            for (Iterator it = results.keySet().iterator(); it.hasNext();) {
+                Object key = it.next();
+                fw.write(key + ":" + results.get(key) + "\n");
+            }
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("Nie znaleziono pliku!");
+        }
+    }
+    
+    public static HashMap readResults() {
+        String line = "";
+        HashMap results = new HashMap();
+        File file = new File("wyniki.txt");
+        FileInputStream fis = null;
+        BufferedReader br = null;
+        try {
+            fis = new FileInputStream(file);
+            br = new BufferedReader(new InputStreamReader(fis));
+            while ((line = br.readLine()) != null) {                
+                String[] elem = line.split(":");
+                results.put(elem[0], Integer.parseInt(elem[1]));
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Blad odczytu pliku!");
+        }
+        return results;
+    }
+}
